614dcb66739bac1cc0d31e1166d14fb81b43a26c
solisoma <solisomamadubueze@gmail.com>
2024-03-22 04:03:49 +0100

    published package

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,10 @@
+<script>
+  let count = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button on:click={increment} class="border-4 border-red-400">
+  count is {count}
+</button>
